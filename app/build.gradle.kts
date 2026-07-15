68aa8fe2edfc979060622c4cba5b9d66226ce266
AgungDH <agungdh@live.com>
2026-07-15 17:35:50 +0700

    chore: bump version to 0.0.5 (versionCode 5)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         minSdk = 29
         //noinspection OldTargetApi
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.0.2"
+        versionCode = 5
+        versionName = "0.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
